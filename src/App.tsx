fda55333a93ec8f3c8e03243e8a87dd81446b2e7
Mavlonjon Mirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-01-19 10:38:11 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { ChangeEvent, useState } from "react";
+import styles from "./home.module.css";
+import { IData } from "./interfaces";
+import { data } from "./constants";
+const App = (): JSX.Element => {
+  const [title, setTitle] = useState<string>();
+  const [arr, setArr] = useState<IData[]>(data);
+
+  const changeHandler = (evt: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(evt.target.value);
+  };
+
+  const handleSubmit = (): void => {
+    if (!title?.length) return;
+    const newData = {
+      title: title,
+      id: new Date().getTime(),
+      description: "description",
+    };
+    setArr([...arr, newData]);
+    setTitle("");
+  };
+
+  const deleteItem = (id: number): void => {
+    const newData = arr.filter((c) => c.id !== id);
+    setArr(newData);
+  };
+
+  return (
+    <div className={styles.todo}>
+      <h1 className={styles.title}>App Todo</h1>
+      <input
+        type="text"
+        placeholder="Enter todo..."
+        value={title}
+        onChange={changeHandler}
+        className={styles.input}
+      />
+      <button className={styles.button} onClick={handleSubmit}>
+        Add Todo
+      </button>
+
+      <div className={styles.card}>
+        {arr.map((c) => (
+          <div key={c.id} className={styles.cardItem}>
+            <p>{c.title}</p>
+            <div className={styles.delBtn}>
+              <button onClick={() => deleteItem(c.id)}>DELETE</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import { IData } from "../interfaces";
+
+export const data: IData[] = [
+  { title: "Omar", id: 1, description: "lorem" },
+  { title: "Osman", id: 2, description: "lorem" },
+  { title: "Abdullah", id: 3, description: "lorem" },
+];
